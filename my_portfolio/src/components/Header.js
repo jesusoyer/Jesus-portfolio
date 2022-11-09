9c99f68d32bf9eb2210b91cda2352972b8e16a36
jesusoyer <oyervidesjesus017@gmail.com>
2022-11-09 15:38:18 -0600

    created header component

---
+++ my_portfolio/src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./HeaderItem.css"
+class Header extends React.Component{
+
+    render(){
+        return(
+            <div class="ui huge header" style={{marginTop:'5%',fontFamily:'Old Standard TT,serif ',fontSize:'3em'}}>JO</div>
+        )
+
+
+    }
+}
+export default Header;
